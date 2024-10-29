c31d594350b67c7ba3cc9a3e5bfaae4f963be2fe
yuandaishi <623926557@example.com>
2024-10-29 11:58:02 +0800

    模拟人工智能对话

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import ChatUI from './page/chatUI';
 import './App.css';
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [showChat, changeChatCon] = useState<string | null | boolean>(localStorage.getItem('chatApiKey'));
+  const [inputError, changeinputError] = useState(false);
+  let handleFocus = () => {
+    changeinputError(false);
+  }
+  let handleClick = () => {
+    if (inputRef.current?.value) {
+      localStorage.setItem('chatApiKey', inputRef.current.value);
+      changeChatCon(true);
+    } else {
+      changeinputError(true);
+    }
+  }
+  if (showChat) {
+    return (
+      <ChatUI resetApi={changeChatCon} />
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="API-key-box">
+      <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+        欢迎使用OpenRoute，请在下方输入您的OpenRouter API key
+      </Box >
+      <TextField required error={inputError} id="outlined-basic" label="API key" variant="outlined" margin="dense" inputRef={inputRef} onFocus={handleFocus} />
+      <Button variant="contained" onClick={handleClick}>确认</Button>
+    </div >
   );
 }
 
---
+++ src/page/chatUI.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useRef, useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Comment from '@mui/icons-material/Comment';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import './chatui.scss';
+import chatgtp from './../images/chatgpt.png';
+
+function ChatUI({ resetApi }: { resetApi: Function }) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [questionArr, setQuestionArr] = useState<any[]>([]);
+    const [answerArr, setAnswerArr] = useState<any[]>([]);
+    useEffect(() => {
+        if (questionArr.length !== 0) {
+            getData();
+            window.scrollTo(0, document.body.scrollHeight);
+        }
+    }, [questionArr])
+    useEffect(() => {
+        window.addEventListener('keypress', keypress);
+        return () => {
+            window.removeEventListener('keypress', keypress);
+        }
+    }, [])
+    useEffect(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+    }, [answerArr])
+    let keypress = (e: KeyboardEvent) => {
+        if (e.keyCode === 13) {
+            handleClickSendMessage();
+        }
+    }
+    let handleClickSendMessage = () => {
+        if (inputRef.current?.value) {
+            setQuestionArr((pre) => ([...pre, inputRef.current?.value]));
+        }
+    }
+    let handleClickReset = () => {
+        localStorage.removeItem('chatApiKey');
+        resetApi(false)
+    }
+    let getData = async () => {
+        let data = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('chatApiKey')}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "model": "mistralai/mistral-7b-instruct:free",
+                "messages": questionArr.map(item => ({ role: "user", content: item })),
+                // "stream": true,
+                "transforms": ["middle-out"],
+                "max_tokens": 0,
+            })
+        })
+        if (data.status !== 200) {
+            setAnswerArr([...answerArr, 'AI出现问题，请稍后询问']);
+            return;
+        }
+        data.json().then((data) => {
+            setAnswerArr([...answerArr, data.choices[0].message.content]);
+        })
+    }
+    return (
+        <div className="ChatUI">
+            <div className='AI-box'>
+                {
+                    questionArr.map((item, key) => {
+                        return (
+                            <div key={`box-${key}`}>
+                                <ListItem alignItems="flex-start" key={`question-${key}`} className='y-list'>
+                                    <ListItemAvatar>
+                                        <Avatar alt="Y" />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary="You"
+                                        secondary={item}
+                                    />
+                                </ListItem>
+                                {
+                                    answerArr[key] ? <ListItem alignItems="flex-start" key={`answer-${key}`}>
+                                        <ListItemAvatar>
+                                            <Avatar alt="chat" src={chatgtp} />
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary="ChatGTP"
+                                            secondary={answerArr[key]}
+                                        />
+                                    </ListItem> : <Box sx={{ display: 'flex', justifyContent: "center" }}>
+                                        <CircularProgress />
+                                    </Box>
+                                }
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='from-contain'>
+                <FormControl sx={{ m: 1, width: '80vw' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Message</InputLabel>
+                    <OutlinedInput
+                        inputRef={inputRef}
+                        id="outlined-adornment-password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={handleClickSendMessage}
+                                    edge="end"
+                                >
+                                    <Comment />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Message ChatGTP..."
+                    />
+                </FormControl>
+            </div>
+            <Button variant="contained" onClick={handleClickReset} className='reset-api'>reset API key</Button>
+        </div>
+    )
+}
+
+export default ChatUI;
